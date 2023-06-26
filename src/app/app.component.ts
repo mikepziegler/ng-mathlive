a5580016dd0b5cd13af65b559f1f615d9b5fa379
mikepziegler <info@mike-ziegler.com>
2023-06-27 01:15:23 +0200

    Add mathlive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { renderMathInElement } from 'mathlive';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `
+    <div id='mathfield'></div>
+
+  `
 })
-export class AppComponent {
-  title = 'ng-mathlive';
+export class AppComponent implements AfterViewInit {
+  @ViewChild('mathField', { static: true })
+  mathFieldElement: ElementRef<HTMLDivElement>;
+
+  mathfield: any;
+  latex: string;
+  mathML: string;
+  asciiMath: string;
+  svg: string;
+
+  ngAfterViewInit() {
+    renderMathInElement('mathfield', {});
+  }
+
+  onCmd(latex: string) {
+    this.mathfield.$perform([
+      'insert',
+      latex,
+      { focus: true, feedback: true, mode: 'math', format: 'auto', resetStyle: true }
+    ]);
+    this.convert();
+  }
+
+  convert() {
+    this.latex = this.mathfield.$text('latex');
+    this.mathML = this.mathfield.$text('mathML');
+  }
 }
