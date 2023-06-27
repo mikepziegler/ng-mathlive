a81dfec7abb8e7d60fc7184bf0efaca7216c0644
mikepziegler <info@mike-ziegler.com>
2023-06-27 09:17:43 +0200

    Functional angular mathlive component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,28 @@
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-import { renderMathInElement } from 'mathlive';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { MathfieldElement } from 'mathlive';
+
+MathfieldElement.fontsDirectory = '../assets/mathlive/fonts';
 
 @Component({
   selector: 'app-root',
   template: `
-    <div id='mathfield'></div>
-
+    <math-field #mathField />
   `
 })
-export class AppComponent implements AfterViewInit {
-  @ViewChild('mathField', { static: true })
-  mathFieldElement: ElementRef<HTMLDivElement>;
+export class AppComponent implements OnInit, AfterViewInit {
+  @ViewChild('mathField', { static: true }) mathFieldElement: ElementRef<MathfieldElement>;
 
-  mathfield: any;
-  latex: string;
-  mathML: string;
-  asciiMath: string;
-  svg: string;
+  ngOnInit() {
+    const mfe = new MathfieldElement();
+
+
+    this.mathFieldElement.nativeElement = mfe;
+
+
+  }
 
   ngAfterViewInit() {
-    renderMathInElement('mathfield', {});
+
   }
 
-  onCmd(latex: string) {
-    this.mathfield.$perform([
-      'insert',
-      latex,
-      { focus: true, feedback: true, mode: 'math', format: 'auto', resetStyle: true }
-    ]);
-    this.convert();
-  }
-
-  convert() {
-    this.latex = this.mathfield.$text('latex');
-    this.mathML = this.mathfield.$text('mathML');
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -10,7 +10,9 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule
   ],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
